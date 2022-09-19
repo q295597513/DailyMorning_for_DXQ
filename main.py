d7461db6c92d33025ebce6a411d565c6c348e0b1
q295597513 <48614705+q295597513@users.noreply.github.com>
2022-09-19 09:13:53 +0800

    调试

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
                 "color": get_color()
             },
             "chp_en": {
-                "value": chp_en + year + month +day,
+                "value": "{} {} {} {}".format(chp_en,year, month,day),
                 "color": get_color()
             }
         }
